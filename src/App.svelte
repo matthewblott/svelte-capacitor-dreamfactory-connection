eab8987dba2761a8bd42e31b6d6573b7ca529a6e
Matthew Blott <m4ttb@pm.me>
2023-02-22 17:38:58 +0000

    saved

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { onMount } from "svelte";
+
+  let base_url = "http://localhost:8000/api/v2";
+
+  const login = async (email, password) => {
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+
+    const credentials = {
+      email: email,
+      password: password,
+    };
+
+    const body = JSON.stringify(credentials);
+    const url = `${base_url}/user/session`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body,
+    });
+
+    if (!response.ok) {
+      console.log("error");
+      return response.statusText;
+    } else {
+      console.log("success");
+      const json = await response.json();
+
+      return json;
+    }
+  };
+  const connect = async () => {
+    const res = await login("read_write@northwi.nd", "password");
+
+    console.log(res);
+  };
+
+  onMount(() => {
+    //
+  });
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+
+  <button on:click={connect}>Connect</button>
+</main>
